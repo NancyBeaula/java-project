28469c37414fd009f7829c0f5b0f93fea281feba
Nancy Beaula <nancyebinezer@gmail.com>
2025-08-30 14:46:20 -0400

    Added new module sample to github
    we checked print statement

---
+++ Sample/src/Main.java
@@ -0,0 +1,6 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sample project - to push to git hub");
+}
